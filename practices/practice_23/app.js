d07a57ef411d3392c1e97b396991b5a63fae5a3f
muntazirmalikshigri <muntazirshigri.12@gmail.com>
2025-05-02 00:33:17 +0500

    practices

---
+++ practices/practice_23/app.js
@@ -0,0 +1,39 @@
+// let msg='    help';
+// newMsg=msg.trim();
+// console.log(newMsg.toUpperCase());
+// console.log(msg.toLowerCase());
+// console.log(msg.trim().toUpperCase())
+// let name="mutazirmalik";
+// console.log(name.slice(2,7).indexOf("u"))
+// console.log(name,name.replace('malik',' munna'))
+// let str='ApnaCollege';
+// console.log(str.slice(4).replace('l','t'))
+// newStr=str.slice(4).replace('l','t')
+// console.log(newStr.replace('l','t'))
+
+
+//                      string
+ 
+// let students=['malik','munna','fakhar','sohail']
+// // console.log(students);
+// console.log(typeof students)
+// let info=['malik',90,6.5,true];
+// info[1]='munna';
+// info.push("good");
+// info.pop("good");
+// info.unshift('start');
+// info.shift('start');
+// console.log(info);
+// let followers=['a','b','c']
+// let blocked=followers.shift();
+// console.log(followers);
+
+let start=['january','july','march','august'];
+console.log(start);
+start.shift('january');
+start.shift('july');
+start.unshift('june');
+start.unshift('july');
+console.log(start);
+
+
---
+++ practices/practice_24/app.js
@@ -0,0 +1,7 @@
+let color=['red','blue','yellow','green','pik'];
+color.splice(1,4,'black','brown');
+color.push('malik');
+color.push('munna');
+color.pop();
+color.pop();
+console.log(color);
